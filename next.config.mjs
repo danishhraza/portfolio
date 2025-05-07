eb8b706b13f56257927039ea4b686100f24a3f31
Danish Raza <danishh.raza05@gmail.com>
2025-05-07 15:24:50 +0500

    deploy azure

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    distDir: 'build',
+    output: 'standalone'
+    // Keep any other existing configurations you already have here
+  };
+  
+  export default nextConfig;
