77bebaf2668e31eb763a2a134cd3a9967f3d7686
Danish Raza <danishh.raza05@gmail.com>
2025-07-07 18:30:58 +0500

    updated next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    distDir: 'build',
-    output: 'standalone'
-    // Keep any other existing configurations you already have here
-  };
-  
-  export default nextConfig;
+const nextConfig = {};
+
+export default nextConfig;
+
+
